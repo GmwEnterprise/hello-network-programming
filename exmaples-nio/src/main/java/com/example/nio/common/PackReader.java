d3627ac800986bf4057d61f0a8ac225868a1956a
Gmw <909498022@qq.com>
2021-08-23 19:03:46 +0800

    ghmw

--- exmaples-nio/src/main/java/com/example/nio/common/PackReader.java
+++ exmaples-nio/src/main/java/com/example/nio/common/PackReader.java
@@ -5,27 +5,57 @@ import java.nio.ByteBuffer;
 
 public class PackReader {
 
-    private final byte[] header = new byte[5];
-    private final ByteArrayOutputStream content = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream byteStore = new ByteArrayOutputStream();
+    private int position = 0;
+    private int contentLen = 0;
 
-    private int pos = 0, contentLen;
+    private Pack pack = null;
+    private boolean finishRead = false;
 
-    public void read(ByteBuffer buf) {
-        if (pos <= 5) {
-            while (pos <= 5 && buf.hasRemaining()) {
-                header[pos] = buf.get();
-                if (pos == 5) {
-                    contentLen = Pack.calculateLen(header);
+    public Pack packPrepared() {
+        return pack;
+    }
+
+    public void readBuffer(ByteBuffer buf) {
+        if (finishRead) {
+            throw new RuntimeException("pack已就绪，请重新创建一个packReader以继续读取");
+        }
+        int bufAvailable = buf.limit() - buf.position();
+        if (position < 5) {
+            int headerNeed = 5 - position;
+            int readLen = Math.min(headerNeed, bufAvailable);
+            byteStore.write(buf.array(), buf.position(), readLen);
+            buf.position(buf.position() + readLen);
+            position += readLen;
+        }
+
+        if (position >= 5) {
+            if (contentLen == 0) {
+                byte[] contentLenByteArray = new byte[4];
+                byte[] byteArray = byteStore.toByteArray();
+                System.arraycopy(byteArray, 1, contentLenByteArray, 0, 4);
+                contentLen = Bytes.byteArrayToInt(contentLenByteArray);
+            }
+
+            if (buf.hasRemaining()) {
+                bufAvailable = buf.limit() - buf.position();
+                int contentNeed = 5 + contentLen - position;
+                int readLen = Math.min(bufAvailable, contentNeed);
+                if (readLen > 0) {
+                    byteStore.write(buf.array(), buf.position(), readLen);
+                    buf.position(buf.position() + readLen);
+                    position += readLen;
                 }
-                pos++;
+            }
+
+            if (contentLen + 5 == position) {
+                // 读完一个pack
+                byte[] bytes = byteStore.toByteArray();
+                byte[] res = new byte[bytes.length - 5];
+                System.arraycopy(bytes, 5, res, 0, res.length);
+                pack = new Pack(res);
+                finishRead = true;
             }
         }
-
-        if (pos > 5) {
-            content.write(buf.array(), buf.position(), buf.limit() - buf.position());
-            buf.position(buf.limit());
-        }
-
-        // if ()
     }
 }
